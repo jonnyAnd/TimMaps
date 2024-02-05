17b4ed89deb3be0c6a9d5a6628db506eed1741f4
jonnyAnd <jonny@eino.co.uk>
2024-02-05 00:00:17 +0000

    release

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-function main(){
-	console.log("hello");
+function main() {
+  console.log("hello");
+
+  //   addExportClick();
 }
+
+function contentClick(contentElement) {
+  console.log("poo" + contentElement);
+
+  makeImage(contentElement);
+
+}
+
+
+
+function makeImage(contentElement){
+	// var container = document.getElementById("htmltoimage");; /* full page */
+	html2canvas(contentElement, { allowTaint: true }).then(function (canvas) {
+
+		var link = document.createElement("a");
+		document.body.appendChild(link);
+		link.download = "html_image.jpg";
+		link.href = canvas.toDataURL();
+		link.target = '_blank';
+		link.click();
+	});
+}
+
+
+
+
 
 
 main();
